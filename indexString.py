60d0a379d2ef453f895370b5ee14ec0670fe256f
vanthang221 <vanthang@aipt.vn>
2025-04-16 08:49:43 +0700

    u

---
+++ indexString.py
@@ -0,0 +1,21 @@
+a = "Hello"
+print(a.upper())  
+print(a.lower())  
+print(a.strip())
+
+b = "Hello"
+c = "World"
+d = b + c
+print(d)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt) 
